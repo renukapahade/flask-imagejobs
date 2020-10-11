34d68c555301f9c619b78c8c7fd8cc96302acfcf
Renuka Pahade <renuka391@gmail.com>
2020-10-11 09:55:58 +0530

    Changed db configuration settings

--- config.py
+++ config.py
@@ -6,14 +6,16 @@ class Config():
     """
     This class loads the settings for the database and constants
     """
-    db_host = None
+    sql_db_host = None
     sql_db_name = None
     sql_username = None
     sql_password = None
+    sql_db_port = None
 
     def __init__(self):
         # Load expected variables into different variables
-        self.db_host = "localhost"
+        self.sql_db_host = "localhost"
         self.sql_db_name = "retail_store"
         self.sql_username = "root"
         self.sql_password = "password"
+        self.sql_db_port = "3306"
--- db_connection.py
+++ db_connection.py
@@ -8,16 +8,21 @@ from sqlalchemy import create_engine
 
 class SqlDbConnect():
 
-    def __init__(self, config_object):
+    def __init__(self, config_object, db_name=None):
         """
         Connect to the MySQL database
         :param config_object: config object that have database parameters
         """
         self.error = None
+
+        # mysql server connection
         try:
-            # mysql server connection
-            engine = create_engine("mysql+pymysql://%s:%s@%s:3306"
-                                   % (config_object.sql_username,config_object.sql_password,config_object.db_host), echo=False)
+            if db_name:
+                engine = create_engine("mysql+pymysql://%s:%s@%s:%s/%s"
+                                    % (config_object.sql_username,config_object.sql_password,config_object.sql_db_host,config_object.sql_db_port,db_name), echo=False)
+            else:
+                engine = create_engine("mysql+pymysql://%s:%s@%s:%s"
+                                    % (config_object.sql_username,config_object.sql_password,config_object.sql_db_host,config_object.sql_db_port), echo=False)
             self.sql_db = engine.connect()
         except Exception as e:
             print(e)
